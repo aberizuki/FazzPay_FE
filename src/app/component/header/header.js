8ab2ba3787f24d86ac046bb7499ce4cbe787b964
aberizuki <zalaarthurn@gmail.com>
2023-02-12 17:02:47 +0700

    update : comps & home

---
+++ src/app/component/header/header.js
@@ -0,0 +1,9 @@
+import Login from "./login";
+import Unlogin from "./unlogin";
+
+export default function Header() {
+  return (
+    <Login />
+    //   <Unlogin/>
+  );
+}
--- tailwind.config.js
+++ tailwind.config.js
@@ -8,6 +8,6 @@ module.exports = {
       },
     },
   },
-  plugins: [require("daisyui")],
-  // plugins: [],
+  // plugins: [require("daisyui")],
+  plugins: [],
 };
